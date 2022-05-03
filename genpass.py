a39ef216e36cbb8539f123474dfd55736d8b9c8e
valstan <123qwemmm@mail.ru>
2022-05-03 23:12:09 +0300

    добавил сохрание паролей в файл

--- genpass.py
+++ genpass.py
@@ -3,8 +3,15 @@
 
 import random
 
+from open_file_json import open_file_json
+from save_file_json import save_file_json
+
+passfile = open_file_json('', 'password')
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 password = ''
-for i in range(10):
+for i in range(15):
     password += random.choice(chars)
 print(password)
+
+passfile['passwords'].append(password)
+save_file_json('', 'password', passfile)
